cb24d93b2a7796fdf3d5785785bf8a671e2fce4b
Edward Mahama <mahamaek@gmail.com>
2025-12-30 16:26:52 -0500

    register route blueprint with api object

--- ch06/kitchen/app.py
+++ ch06/kitchen/app.py
@@ -2,8 +2,10 @@
 from flask import Flask
 from flask_smorest import Api
 from config import BaseConfig
+from api.api import blueprint
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 
 kitchen_api = Api(app)
+kitchen_api.register_blueprint(blueprint)
