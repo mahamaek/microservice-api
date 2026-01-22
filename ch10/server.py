f3488d56bd0844965d095b2a37a0016e8edf4333
Edward Mahama <mahamaek@gmail.com>
2026-01-22 08:51:54 -0500

    add the entry point for server

--- ch10/server.py
+++ ch10/server.py
@@ -2,19 +2,6 @@ from ariadne import make_executable_schema, gql, QueryType
 from ariadne.asgi import GraphQL
 import random
 import string
+from web.schema import schema
 
-schema = """
-    type Query {
-        hello: String
-}
-"""
-
-query = QueryType()
-
-
-@query.field('hello')
-def resolve_hello(*_):
-    return ''.join(random.choice(string.ascii_letters) for _ in range(10))
-
-
-server = GraphQL(make_executable_schema(schema, [query]), debug=True)
+server = GraphQL(schema,debug=True)
