8ee7928c431f6fa44598e6db6d9c6e1d5201cd6d
Edward Mahama <mahamaek@gmail.com>
2026-01-22 08:52:26 -0500

    add query resolvers

---
+++ ch10/web/queries.py
@@ -0,0 +1,69 @@
+from ariadne import QueryType
+from web.data import ingredients, products
+from itertools import islice
+from excepions import ItemNotFoundError
+
+query = QueryType()
+
+
+def get_page(items, items_per_page, page):
+    page = page - 1
+    start = items_per_page*page if page > 0 else page
+    stop = start + items_per_page
+    return list(islice(items, start, stop))
+
+
+@query.field('allIngredients')
+def resolve_all_ingredients(*_):
+    return ingredients
+
+
+@query.field('allProducts')
+def resolve_all_products(*_):
+    return products
+
+
+@query.field('products')
+def resolve_products(*_, input=None):
+    filtered = [product for product in products]
+    if input is None:
+        return filtered
+
+    filtered = [
+        product for product in filtered
+        if product['available'] is input['available']
+    ]
+
+    if input.get('minPrice') is not None:
+        filtered = [
+            product for product in filtered
+            if product['price'] >= input['minPrice']
+        ]
+    if input.get('maxPrice') is not None:
+        filtered = [
+            product for product in filtered
+            if product['price'] <= input['maxPrice']
+        ]
+
+    filtered.sort(
+        key=lambda product: product.get(input['sortBy'], 0),
+        reverse=input['sort'] == 'DESCENDING'
+    )
+
+    return get_page(filtered, input['resultsPerPage'], input['page'])
+
+
+@query.field('product')
+def resolve_product(*_, id):
+    for product in products:
+        if product['id'] == id:
+            return product
+    return ItemNotFoundError(f'Product with ID {id} not found')
+
+
+@query.field('ingredient')
+def resolve_ingredient(*_, id):
+    for ingredient in ingredients:
+        if ingredient['id'] == id:
+            return ingredient
+    return ItemNotFoundError(f'Ingredient with ID {id} not found')
