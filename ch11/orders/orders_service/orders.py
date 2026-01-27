1d3c27a2af10147fae80b7dda76b2641c67b91a8
Edward Mahama <mahamaek@gmail.com>
2026-01-27 08:04:12 -0500

    copy ch07 to ch11

---
+++ ch11/orders/orders_service/orders.py
@@ -0,0 +1,83 @@
+import requests
+
+from exceptions import APIIntegrationError, InvalidActionError
+
+
+class OrderItem:
+    def __init__(self, id, product, quantity, size):
+        self.id = id
+        self.product = product
+        self.quantity = quantity
+        self.size = size
+
+
+class Order:
+    def __init__(self, id, created, items, status, schedule_id=None, delivery_id=None, order_=None):
+        self._id = id
+        self._order = order_
+        self._created = created
+        self.items = items
+        self._status = status
+        self.schedule_id = schedule_id
+        self.delivery_id = delivery_id
+
+    @property
+    def id(self):
+        return self._id or self.order_.id
+
+    @property
+    def created(self):
+        return self._created or self.order_.created
+
+    def status(self):
+        return self._status or self.order_.status
+
+    def cancel(self):
+        if self.status == 'progress':
+            response = requests.post(
+                f'http://localhost:3000/kitchen/schedules/{self.schedule_id}/cancel',
+                json={'order': [item.dict() for item in self.items]}
+            )
+            if response.status_code == 200:
+                return
+            raise APIIntegrationError(
+                f'Could not cancel order with id {self.id}'
+            )
+        if self.status == 'delivery':
+            raise InvalidActionError(f'Cannot cancel order with id {self.id}')
+
+    def pay(self):
+        response = requests.post(
+            'http://localhost:3001/payments', json={'order_id': self.id}
+        )
+        if response.status_code == 201:
+            return
+        raise APIIntegrationError(
+            f'Could not process payment for order with id {self.id}'
+        )
+
+    def schedule(self):
+        response = requests.post(
+            'http://localhost:3000/kitchen/schedules',
+            json={'order': [item.dict() for item in self.items]}
+        )
+        if response.status_code == 201:
+            return response.json()['id']
+        raise APIIntegrationError(
+            f'Could not schedule order with id {self.id}'
+        )
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'order': [item.dict() for item in self.items],
+            'status': self.status,
+            'created': self.created,
+        }
+# using prisim to mock api servier
+
+# ./node_modules/.bin/prism mock ./ch07/kitchen.yaml --port 3000
+# ./node_modules/.bin/prism mock ./ch07/payment.yaml --port 3001
+
+
+# curl http://localhost:3000/kitchen/schedules | jq
---
+++ ch11/orders/web/api/schemas.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from enum import Enum
+from typing import Annotated, List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field, conlist, conint, field_validator, ConfigDict
+
+
+class Size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class Status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Annotated[int, Field(strict=True, ge=1)] = 1
+
+    model_config = ConfigDict(extra='forbid')
+
+    @field_validator('quantity')
+    def quantity_non_nullable(cls, value):
+        if value is None:
+            raise ValueError('quantity may not be None')
+        return value
+
+
+class CreateOrderSchema(BaseModel):
+    order: Annotated[List[OrderItemSchema], Field(min_items=1)]
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+
+class GetOrdersSchema(BaseModel):
+    # orders: Annotated[List[GetOrderSchema], Field(min_items=0)]
+    orders: List[GetOrderSchema]
