4fe4c1025b79f2c1b1c6c1d89cf2488cb8141ae6
Edward Mahama <mahamaek@gmail.com>
2026-02-04 21:45:44 -0500

    register middleware

--- ch11/orders/web/app.py
+++ ch11/orders/web/app.py
@@ -1,7 +1,12 @@
-from orders.web.api import api
+from orders.web.middlewares.authorization_middleware import AuthorizeRequestMiddleware
 from pathlib import Path
 import yaml
 from fastapi import FastAPI
+
+from starlette.middleware.cors import CORSMiddleware
+
+
+from orders.web.api.auth import decode_and_validate_token
 
 app = FastAPI(debug=True, openapi_url="/openapi/orders.json",
               docs_url="/docs/orders")
@@ -10,3 +15,15 @@ oas_doc = yaml.safe_load(
     (Path(__file__).parent / "../../oas.yaml").read_text())
 
 app.openapi = lambda: oas_doc
+
+app.add_middleware(AuthorizeRequestMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from orders.web.api import api  # isort:skip  # noqa: E402
