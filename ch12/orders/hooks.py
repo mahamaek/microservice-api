ef12af3a59272d5338a6648cf7491c0377ba1749
Edward Mahama <mahamaek@gmail.com>
2026-02-07 21:38:18 -0500

    add a hook to save id of created order

---
+++ ch12/orders/hooks.py
@@ -0,0 +1,11 @@
+import json
+import dredd_hooks
+
+response_stash = {}
+
+
+@dredd_hooks.after('/orders > Creates an order > 200 > pplication/json')
+def save_created_order(transaction):
+    response_payload = transaction['real']['body']
+    order_id = json.loads(response_payload)
+    response_stash['created_order_id'] = order_id
