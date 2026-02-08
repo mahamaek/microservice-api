a201e8dcf49fbe9b78cedadd054f196d29d7cdd3
Edward Mahama <mahamaek@gmail.com>
2026-02-07 21:52:29 -0500

    adding before hook before running get, put and delete

--- ch12/orders/hooks.py
+++ ch12/orders/hooks.py
@@ -4,8 +4,26 @@ import dredd_hooks
 response_stash = {}
 
 
-@dredd_hooks.after('/orders > Creates an order > 200 > pplication/json')
+@dredd_hooks.after('/orders > Creates an order > 200 > application/json')
 def save_created_order(transaction):
     response_payload = transaction['real']['body']
     order_id = json.loads(response_payload)
     response_stash['created_order_id'] = order_id
+
+
+@dredd_hooks.before('/orders/{order_id} > Returns the details of a specific order > 200 > application/json')
+def before_get_order(transaction):
+    transaction["fullPath"] = f"orders/{response_stash['created_order_id']}"
+    transaction["request"]["uri"] = f"orders/{response_stash['created_order_id']}"
+
+
+@dredd_hooks.before("/orders/{order_id} > Replaces an existing order > 200 > " "application/json")
+def before_put_order(transaction):
+    transaction["fullPath"] = f"orders/{response_stash['created_order_id']}"
+    transaction["request"]["uri"] = f"orders/{response_stash['created_order_id']}"
+
+
+@dredd_hooks.before("/orders/{order_id} > Deletes an existing order > 204")
+def before_delete_order(transaction):
+    transaction["fullPath"] = f"orders/{response_stash['created_order_id']}"
+    transaction["request"]["uri"] = f"orders/{response_stash['created_order_id']}"
